28cccde83ea0590954f65b3671ec3173d6026d66
Moritz Ulrich <moritz@tarn-vedra.de>
2019-04-28 16:21:46 +0200

    Add Identity agent

---
+++ lib/identity.ex
@@ -0,0 +1,15 @@
+defmodule Sailor.Identity do
+  use Agent
+
+  def start_link([identity, network_identifier]) do
+    Agent.start_link(fn -> {identity, network_identifier} end, name: __MODULE__)
+  end
+
+  def keypair do
+    Agent.get(__MODULE__, fn {identity, _} -> identity end)
+  end
+
+  def network_identifier do
+    Agent.get(__MODULE__, fn {_, network_identifier} -> network_identifier end)
+  end
+end
