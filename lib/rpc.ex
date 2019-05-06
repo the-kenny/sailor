b02baf11fdb8d858639d634e8c754d5e2e5b1f8b
Moritz Ulrich <moritz@tarn-vedra.de>
2019-05-06 11:26:03 +0200

    Write fluent api for Rpc.Packet

--- lib/rpc.ex
+++ lib/rpc.ex
@@ -1,11 +1,12 @@
 defmodule Sailor.Rpc do
-  use GenServer
+
+  # use GenServer
 
   # defprotocol Handler do
-  #   def handle_
-  # end
+    #   def handle_
+    # end
 
-  defmodule Packet do
+    defmodule Packet do
     # @spec parse(binary()) :: {:ok, struct(), binary()} | :incomplete | {:incomplete, integer()} | {:error, :invalid}
 
     # def parse(binary) when byte_size(binary) < 9 do
@@ -21,26 +22,125 @@ defmodule Sailor.Rpc do
     #   end
     # end
 
+    defmacro packet_binary do
+      quote generated: true do
+        <<
+        0 :: 4,
+        var!(stream?) :: 1,
+        var!(end_or_error?) :: 1,
+        var!(body_type) :: 2,
+        var!(body_length) :: unsigned-32,
+        var!(request_number) :: signed-32,
+        var!(body) :: binary,
+      >>
+      end
+    end
+
+    @doc """
+    Creates an empty packet with all flags set to zero, packet number 0 and body length 0
+    ## Examples
+
+      iex> create() |> body_length()
+      0
+    """
+    def create() do
+      stream? = 0
+      end_or_error? = 0
+      body_type = 0
+      body_length = 0
+      request_number = 0
+      body = <<>>
+      packet_binary()
+    end
+
+    @doc """
+    ## Examples
+
+      iex> create() |> stream() |> stream?()
+      true
+    """
+    def stream(packet) do
+      packet_binary() = packet
+      stream? = 1
+      packet_binary()
+    end
+
+    def async(packet) do
+      packet_binary() = packet
+      stream? = 0
+      packet_binary()
+    end
+
+    def stream?(packet) do
+      packet_binary() = packet
+      stream? == 1
+    end
+
+    def request_number(packet, new_request_number) do
+      packet_binary() = packet
+      request_number = new_request_number
+      packet_binary()
+    end
+
+    def request_number(packet) do
+      packet_binary() = packet
+      request_number
+    end
+
+    def body_type(packet) do
+      packet_binary() = packet
+      case body_type do
+        0 -> :binary
+        1 -> :utf8
+        2 -> :json
+      end
+    end
+
+    def body_type(packet, new_body_type) do
+      packet_binary() = packet
+      body_type = case new_body_type do
+        :binary -> 0
+        :utf8 -> 1
+        :json -> 2
+      end
+      packet_binary()
+    end
+
+    def end_or_error?(packet) do
+      packet_binary() = packet
+      end_or_error? == 1
+    end
+
+
+    def body_length(packet) do
+      packet_binary() = packet
+      body_length
+    end
+
+    def body(packet) do
+      packet_binary() = packet
+      body
+    end
+
+    def body(packet, new_body) do
+      packet_binary() = packet
+      body_length = byte_size(new_body)
+      body = new_body
+      packet_binary()
+    end
 
     @doc """
     Hello world.
 
     ## Examples
 
-        iex> packet = Sailor.Rpc.Packet.stream(42, :utf8, "HELLO")
-        iex> Sailor.Rpc.Packet.info(packet)
-        {42, :stream, false, :utf8, 5}
+        iex> packet = create() |> stream() |> request_number(42) |> body_type(:utf8) |> body("HELLO")
+        iex> info(packet)
+        {42, :stream, false, :utf8, 5, "HELLO"}
 
     """
-    def info(<<header :: binary-9, _body :: binary>>) do
-      <<
-        0 :: 4,
-        stream? :: 1,
-        end_or_error? :: 1,
-        body_type :: 2,
-        body_length :: unsigned-32,
-        request_number :: signed-32,
-      >> = header
+    def info(packet) do
+      packet_binary() = packet
 
       body_type = case body_type do
         0 -> :binary
@@ -54,73 +154,8 @@ defmodule Sailor.Rpc do
         end_or_error? == 1,
         body_type,
         body_length,
+        body
       }
-    end
-
-    # TODO: Write a fluent API to create packets
-
-    def stream(request_number, content_type, body) do
-      content_type = case content_type do
-        :binary -> <<0, 0>>
-        :utf8 ->   <<0, 1>>
-        :json ->   <<1, 0>>
-      end |> :binary.decode_unsigned
-      <<
-        0 :: 4,
-        1 :: 1, # This is a stream
-        0 :: 1, # This is not an end or error
-        content_type :: 2,
-        byte_size(body) :: 32,
-        request_number :: signed-32,
-        body :: binary
-      >>
-    end
-
-    # TODO: Reimplement all following in terms of `Packet.info`
-
-    def body_length(packet) do
-      <<_flags :: 8, body_length :: integer-32, _request_number :: 32, _body :: binary>> = packet
-      body_length
-    end
-
-    def request_number(packet) do
-      <<_flags :: 8, _body_length :: 32, request_number :: signed-integer-32, _body :: binary>> = packet
-      request_number
-    end
-
-    # defmacro header() do
-    #   quote do: <<
-    #     0 :: 4,
-    #     var!(stream?) :: 1,
-    #     var!(end_or_error?) :: 1,
-    #     var!(body_type) :: 2,
-    #     var!(body_length) :: integer-32,
-    #     var!(request_number) :: integer-32
-    #   >>
-    # end
-
-    def end_or_error?(packet) do
-      {_, end_or_error?, _} = flags(packet)
-      end_or_error?
-    end
-
-    def stream?(packet) do
-      {stream?, _, _} = flags(packet)
-      stream?
-    end
-
-    def body_type(packet) do
-      {_, _, body_type} = flags(packet)
-      case body_type do
-        0 -> :binary
-        1 -> :utf8
-        2 -> :json
-      end
-    end
-
-    defp flags(<<flags :: bits-8, _rest :: binary>>) do
-      <<0 :: 4, stream? :: 1, end_or_error? :: 1, body_type :: 2>> = flags
-      {stream? == 1, end_or_error? == 1, body_type}
     end
   end
 end
