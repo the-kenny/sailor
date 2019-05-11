c61cf5c60cf88e0694ae1dd446c25a377c43eb7f
Moritz Ulrich <moritz@tarn-vedra.de>
2019-05-12 00:55:48 +0200

    Implement a first version of blob.createWants

--- lib/peer.ex
+++ lib/peer.ex
@@ -22,12 +22,9 @@ defmodule Sailor.Peer do
   def handle_rpc_request(packet, state) do
     request_number = Packet.request_number(packet)
 
-    if Packet.body_type(packet) != :json do
-      Logger.warn "Unknown RPC message #{request_number} of type: #{inspect Packet.body_type(packet)}: #{inspect Packet.body(packet)}"
-      {:noreply, state}
-    else
-      {:ok, %{"name" => name, "type" => type, "args" => args}} = Jason.decode(Packet.body(packet))
-
+    with :json <- Packet.body_type(packet),
+         {:ok, %{"name" => name, "type" => type, "args" => args}} <- Jason.decode(Packet.body(packet))
+    do
       # This check has a race condition, but we only use it for logging. Nothing to worry about.
       if Registry.lookup(Sailor.Rpc.HandlerRegistry, name) == [] do
         Logger.warn "No handler found for #{inspect name}, we MAY not be able to answer request #{request_number}"
@@ -38,13 +35,16 @@ defmodule Sailor.Peer do
           Process.send(handler, {:rpc_request, name, type, args, packet, state.rpc}, [])
         end)
       end)
-      {:noreply, state}
+    else
+      _ -> Logger.warn "Unknown RPC message #{request_number} of type: #{inspect Packet.body_type(packet)}: #{inspect Packet.body(packet)}"
     end
+
+    {:noreply, state}
   end
 
   def handle_rpc_response(packet, state) do
     packet = Packet.info(packet)
-    Logger.info "Received RPC response to #{-packet.request_number}: #{inspect packet.body}"
+    Logger.info "Received RPC response of #{packet.request_number}: #{inspect packet.body}"
     {:noreply, state}
   end
 
@@ -68,8 +68,9 @@ defmodule Sailor.Peer do
 
     {:ok, rpc} = Sailor.Rpc.subscribe_link([reader, writer])
 
-    :ok = Sailor.Rpc.call(rpc, ["createHistoryStream"], :source, [%{id: state.identifier, live: true, old: true}])
-    :ok = Sailor.Rpc.call(rpc, ["blobs", "has"], :async, ["&F9tH7Ci4f1AVK45S9YhV+tK0tsmkTjQLSe5kQ6nEAuo=.sha256"])
+    # :ok = Sailor.Rpc.call(rpc, ["createHistoryStream"], :source, [%{id: state.identifier, live: true, old: true}])
+    # :ok = Sailor.Rpc.call(rpc, ["blobs", "has"], :async, ["&F9tH7Ci4f1AVK45S9YhV+tK0tsmkTjQLSe5kQ6nEAuo=.sha256"])
+    :ok = Sailor.Rpc.call(rpc, ["blobs", "createWants"], :source, [])
 
     {:noreply, %{state | rpc: rpc}}
   end
@@ -77,10 +78,10 @@ defmodule Sailor.Peer do
   def handle_info({:rpc, rpc_packet}, state) do
     case Packet.request_number(rpc_packet) do
       n when n < 0 ->
-        Logger.debug "Dispatching rpc response #{-n}: #{inspect rpc_packet}"
+        # Logger.debug "Dispatching rpc response #{-n}: #{inspect rpc_packet}"
         handle_rpc_response(rpc_packet, state)
       n when n > 0 ->
-        Logger.debug "Dispatching rpc request #{n}: #{inspect rpc_packet}"
+        # Logger.debug "Dispatching rpc request #{n}: #{inspect Packet.body(rpc_packet)}"
         handle_rpc_request(rpc_packet, state)
     end
   end
--- lib/rpc/handler/blobs.ex
+++ lib/rpc/handler/blobs.ex
@@ -4,6 +4,11 @@ defmodule Sailor.Rpc.Handler.Blobs do
 
   alias Sailor.Rpc.Packet
 
+  @supported_rpcs [
+    ["blobs", "has"],
+    ["blobs", "createWants"],
+  ]
+
   def start_link([blobs_path]) do
     GenServer.start_link(__MODULE__, [blobs_path], name: __MODULE__)
   end
@@ -15,7 +20,7 @@ defmodule Sailor.Rpc.Handler.Blobs do
   end
 
   def handle_continue(:register_as_handler, blobs_path) do
-    Sailor.Rpc.HandlerRegistry.register_handler(["blobs", "has"], self())
+    Enum.each(@supported_rpcs, &Sailor.Rpc.HandlerRegistry.register_handler(&1, self()))
     {:noreply, blobs_path}
   end
 
@@ -27,6 +32,19 @@ defmodule Sailor.Rpc.Handler.Blobs do
     |> Packet.body(Jason.encode!(false))
 
     Sailor.Rpc.respond(rpc, packet)
+
+    {:noreply, state}
+  end
+
+  def handle_info({:rpc_request, ["blobs", "createWants"], "source", [], request_packet, rpc}, state) do
+    responses = [
+      %{"&jqp3ImUpZZ4QD/AcST54J24aGaB3lJg5IDG82TeBmN4=.sha256": -1},
+      %{"&/zFse6nZCK5eOuaAz/NrPj929olH2TMZ5ovGZBZzoQU=.sha256": -1},
+    ]
+
+    responses
+    |> Enum.map(fn response -> Packet.respond(request_packet) |> Packet.body_type(:json) |> Packet.body(Jason.encode!(response)) end)
+    |> Enum.each(&Sailor.Rpc.respond(rpc, &1))
 
     {:noreply, state}
   end
