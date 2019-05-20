ef0953ae3a67b57894984c352c101ed0e5c7d6eb
Moritz Ulrich <moritz@tarn-vedra.de>
2019-05-20 12:06:19 +0200

    Message: Add more tests and simplify

--- lib/message.ex
+++ lib/message.ex
@@ -1,4 +1,6 @@
 defmodule Sailor.Message do
+  require Logger
+
   [:previous, :author, :sequence, :timestamp, :hash, :content, :signature] |> Enum.with_index() |> Enum.each(fn {field, idx} ->
     # As the order of fields in a message always stays the same we can use index-access in our proplist.
     # In our getters we use pattern matching with the first part of the key-value tuple to verify we're
@@ -22,7 +24,19 @@ defmodule Sailor.Message do
   end
 
   def from_json(str) do
-    {list} = :jsone.decode(str, object_format: :tuple)
+    list = :jsone.decode(str, object_format: :proplist)
+    # If we get a json string with `key` and `value` (as from `createHistoryStream`) we validate if
+    # the computed and the received message IDs are equal and raise if not. Otherwise we just return
+    # the message.
+    list = case :proplists.get_value("key", list) do
+      :undefined -> list
+      message_id ->
+        message = :proplists.get_value("value", list)
+        if id(message) != message_id do
+          raise RuntimeError, message: "Received message id #{message_id} is not equal to computed message id #{id(message)}"
+        end
+        message
+    end
     {:ok, list}
   end
 
@@ -56,10 +70,10 @@ defmodule Sailor.Message do
     to_json_string(message)
   end
 
-  def message_id(message) do
+  def id(message) do
     {:ok, hash} = hash_string(message)
     |> Salty.Hash.Sha256.hash()
 
-    {:ok, "%#{Base.encode64(hash)}.sha256"}
+    "%#{Base.encode64(hash)}.sha256"
   end
 end
--- test/message_test.exs
+++ test/message_test.exs
@@ -4,40 +4,53 @@ defmodule Sailor.MessageTest do
   alias Sailor.Message
   alias Sailor.Keypair
 
-  @msg ~s({
-    "previous": "%XphMUkWQtomKjXQvFGfsGYpt69sgEY7Y4Vou9cEuJho=.sha256",
-    "author": "@FCX/tsDLpubCPKKfIrw4gc+SQkHcaD17s7GI6i/ziWY=.ed25519",
-    "sequence": 2,
-    "timestamp": 1514517078157,
-    "hash": "sha256",
-    "content": {
-      "type": "post",
-      "text": "Second post!"
-    },
-    "signature": "z7W1ERg9UYZjNfE72ZwEuJF79khG+eOHWFp6iF+KLuSrw8Lqa6IousK4cCn9T5qFa8E14GVek4cAMmMbjqDnAg==.sig.ed25519"
-  })
-
-  @msg2 ~s({
-      "previous": null,
+  @msgs [
+    {"simple msg", ~s({
+      "previous": "%XphMUkWQtomKjXQvFGfsGYpt69sgEY7Y4Vou9cEuJho=.sha256",
       "author": "@FCX/tsDLpubCPKKfIrw4gc+SQkHcaD17s7GI6i/ziWY=.ed25519",
-      "sequence": 1,
-      "timestamp": 1514517067954,
+      "sequence": 2,
+      "timestamp": 1514517078157,
       "hash": "sha256",
       "content": {
         "type": "post",
-        "text": "This is the first post!"
+        "text": "Second post!"
       },
-      "signature": "QYOR/zU9dxE1aKBaxc3C0DJ4gRyZtlMfPLt+CGJcY73sv5abKKKxr1SqhOvnm8TY784VHE8kZHCD8RdzFl1tBA==.sig.ed25519"
-    })
+      "signature": "z7W1ERg9UYZjNfE72ZwEuJF79khG+eOHWFp6iF+KLuSrw8Lqa6IousK4cCn9T5qFa8E14GVek4cAMmMbjqDnAg==.sig.ed25519"
+    })},
+
+    {"msg with key, value", ~s({
+      "key": "%XphMUkWQtomKjXQvFGfsGYpt69sgEY7Y4Vou9cEuJho=.sha256",
+      "value": {
+        "previous": null,
+        "author": "@FCX/tsDLpubCPKKfIrw4gc+SQkHcaD17s7GI6i/ziWY=.ed25519",
+        "sequence": 1,
+        "timestamp": 1514517067954,
+        "hash": "sha256",
+        "content": {
+          "type": "post",
+          "text": "This is the first post!"
+        },
+        "signature": "QYOR/zU9dxE1aKBaxc3C0DJ4gRyZtlMfPLt+CGJcY73sv5abKKKxr1SqhOvnm8TY784VHE8kZHCD8RdzFl1tBA==.sig.ed25519"
+      },
+      "timestamp": 1514517067956
+    })}
+  ]
 
   test "Message.message_id" do
-    {:ok, message} = Message.from_json(@msg)
-    assert {:ok, "%R7lJEkz27lNijPhYNDzYoPjM0Fp+bFWzwX0SmNJB/ZE=.sha256"} = Message.message_id(message)
+    {:ok, message} = Message.from_json(List.keyfind(@msgs, "simple msg", 0) |> elem(1))
+    assert "%R7lJEkz27lNijPhYNDzYoPjM0Fp+bFWzwX0SmNJB/ZE=.sha256" = Message.id(message)
   end
 
-  Enum.each [@msg, @msg2], fn msg ->
+  Enum.each @msgs, fn {name, msg} ->
     @msg msg
-    test "Message.verify_signature for message with signature" <> Map.get(Jason.decode!(@msg), "signature") do
+    test "Message.from_json for " <> name  do
+      {:ok, _message} = Message.from_json(@msg)
+    end
+  end
+
+  Enum.each @msgs, fn {name, msg} ->
+    @msg msg
+    test "Message.verify_signature for message with signature " <> name do
       {:ok, message} = Message.from_json(@msg)
       {:ok, author_identity} = Keypair.from_identifier(Message.author(message))
       assert :ok = Message.verify_signature(message, author_identity)
