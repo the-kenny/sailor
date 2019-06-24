0ad44c49aafa6f7db87e713592cd479c8238db54
Moritz Ulrich <moritz@tarn-vedra.de>
2019-06-24 23:45:47 +0200

    Inline PoolWorker

--- lib/db.ex
+++ lib/db.ex
@@ -26,3 +26,24 @@ defmodule Sailor.Db do
     end)
   end
 end
+
+defmodule Sailor.Db.PoolWorker do
+  use GenServer
+
+  require Logger
+
+  def start_link([db_path]) do
+    GenServer.start_link(__MODULE__, [db_path])
+  end
+
+  def init([db_path]) do
+    {:ok, db} = Sqlitex.open(db_path)
+    :ok = Sqlitex.exec(db, "PRAGMA foreign_keys = ON;")
+    {:ok, db}
+  end
+
+  def handle_call({:exec, fun}, _from, db) do
+    result = apply(fun, [db])
+    {:reply, result, db}
+  end
+end
